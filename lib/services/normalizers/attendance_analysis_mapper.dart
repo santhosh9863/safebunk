6c3572d4727ca5da29ed5ded2471c36f03644ce4
Santhosh <your@gmail.com>
2026-05-16 02:09:32 +0530

    update

--- lib/services/normalizers/attendance_analysis_mapper.dart
+++ lib/services/normalizers/attendance_analysis_mapper.dart
@@ -1,33 +1,30 @@
 import '../../core/calculations/attendance_analysis.dart';
 import '../../core/calculations/attendance_engine.dart';
+import '../../core/calculations/attendance_rules.dart';
 import '../../models/api/course_attendance_model.dart';
 
 class AttendanceAnalysisMapper {
   AttendanceAnalysisMapper._();
 
   static AttendanceAnalysis mapCourseAttendance(CourseAttendanceModel model) {
-    final subj = SubjectAttendance(
-      subjectName: model.subjectName,
-      present: model.totalPresent,
-      absent: model.totalAbsent,
-      leave: model.totalLeave,
-      dutyLeave: model.totalDutyLeave,
-      totalHours: model.totalAttendance,
-    );
-
-    final stats = AttendanceEngine.computeStats(subj);
-
     return AttendanceAnalysis(
-      percentage: stats.percentage,
-      safeBunks: stats.safeBunks,
-      requiredClasses: stats.classesNeeded,
-      isDanger: stats.isDanger,
-      isWarning: stats.isWarning,
-      isSafe: stats.isSafe,
-      presentHours: stats.presentHours,
-      totalHours: stats.totalHours,
-      dutyLeaveHours: stats.dutyLeaveHours,
-      attendanceWithoutDutyLeave: stats.attendanceWithoutDutyLeave,
+      percentage: model.attendancePercentage,
+      safeBunks: AttendanceEngine.calculateSafeBunks(
+        model.totalPresent,
+        model.totalAttendance,
+      ),
+      requiredClasses: AttendanceEngine.calculateRequiredClasses(
+        model.totalPresent,
+        model.totalAttendance,
+      ),
+      isDanger: model.attendancePercentage < AttendanceRules.dangerAttendance,
+      isWarning: model.attendancePercentage >= AttendanceRules.dangerAttendance &&
+          model.attendancePercentage < AttendanceRules.warningAttendance,
+      isSafe: model.attendancePercentage >= AttendanceRules.warningAttendance,
+      presentHours: model.totalPresent,
+      totalHours: model.totalAttendance,
+      dutyLeaveHours: model.totalDutyLeave,
+      attendanceWithoutDutyLeave: model.attendancePercentageWithoutDutyLeave,
     );
   }
 }
